67c3c7a15dabe34440abd7842ce80eeac76ca72f
1rachit-tech <kushwaharajkumaree@gmail.com>
2026-08-26 21:45:47 +0530

    fix: replace generated MyApp test with OJAS test

---
+++ test/widget_test.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:ojas/main.dart';
+
+void main() {
+  test('theme controller changes theme mode', () {
+    final controller = ThemeController();
+
+    expect(controller.themeMode, ThemeMode.system);
+
+    controller.setThemeMode(ThemeMode.dark);
+
+    expect(controller.themeMode, ThemeMode.dark);
+  });
+}
